fd6eda1de9dbf2c34f94e4d491119a0edefcc4c8
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-03-16 11:06:45 -0300

    Update longestPalindromicSubsequence.cpp

--- Dynamic Programming/longestPalindromicSubsequence.cpp
+++ Dynamic Programming/longestPalindromicSubsequence.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int longestPalindromeSubseq(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int> (n));
+        vector<vector<int> > dp(n, vector<int> (n, 1));
 
-        for(int i = 0; i < n; i++) dp[i][i] = 1;
+        for(int i = 0; i < n-1; i++)
+            if(s[i] == s[i+1]) dp[i][i+1] = 2;
 
-        for(int sz = 2; sz <= n; sz++){
-            for(int i = 0; i < n - sz + 1; i++){
-                int j = i + sz - 1;
-                if(s[i] == s[j] && sz == 2) dp[i][j] = 2;
-                else if(s[i] == s[j]) dp[i][j] = dp[i+1][j-1] + 2;
+        for(int sz = 2; sz < n; sz++){
+            for(int i = 0; i + sz < n; i++){
+                int j = i + sz;
+                if(s[i] == s[j]) dp[i][j] = dp[i+1][j-1] + 2;
                 else dp[i][j] = max(dp[i][j-1], dp[i+1][j]);
             }
         }
