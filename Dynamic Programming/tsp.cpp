786ad4035958ca81117496138f305afaa1f5bcbb
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-03-23 19:18:42 -0300

    Create tsp.cpp

---
+++ Dynamic Programming/tsp.cpp
@@ -0,0 +1,39 @@
+ll mask, n, m;
+ll dp[1<<LIM][LIM];
+
+int numOn(ll mask){
+    int resp = 0;
+    for(int i = 0; i < n; i++)
+        if(mask & (1 << i)) resp++;
+    return resp;
+}
+
+ll tsp(int mask, int last){
+    if(numOn(mask) == m) return 0;
+    if(dp[mask][last] != -1) return dp[mask][last];
+    ll ans = 0;
+    for(int i = 0; i < n; i++){
+        if(!(mask & (1 << i))){
+            int new_mask = (mask | (1 << i));
+            ans = max(ans, tsp(new_mask, i) + bonus[last][i] + v[i]);
+        }
+    }
+    return dp[mask][last] = ans;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n >> m >> k;
+  
+    memset(dp, -1, sizeof(dp));
+
+    ll ans = 0;
+    for(int i=0; i<n; i++){
+        ans = max(ans, tsp(1<<i, i));
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
