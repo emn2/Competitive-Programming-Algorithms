780cd295a5d6653dd97da83183e38601a6b9f36a
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-03-28 14:03:36 -0300

    Create fexp.cpp

---
+++ Math/fexp.cpp
@@ -0,0 +1,24 @@
+long long fexp(long long base, long long exp, long long mod){
+    long long ans = 1;
+    while(exp > 0){
+        if(exp & 1){
+            ans = (ans * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp >>= 1;
+    }
+    return ans;
+}
+
+int main(){
+    int t;
+    scanf("%d\n", &t);
+    while(t--){
+        long long a, b, r;
+        scanf("%lld %lld\n", &a, &b);
+        r = fexp(a, b, 1e9+7);
+        printf("%lld\n", r);
+    }
+
+    return 0;
+}
