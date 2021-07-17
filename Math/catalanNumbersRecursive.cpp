d5459013bb82519abd4b4a27f012bedb74a877ec
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-07-17 20:46:57 -0300

    Create catalanNumbersRecursive.cpp

---
+++ Math/catalanNumbersRecursive.cpp
@@ -0,0 +1,12 @@
+void init(){
+    catalan[0] = catalan[1] = 1;
+    for(int i = 2; i < MAXN; i++){
+        catalan[i] = 0;
+        for(int j = 0; j < i; j++){
+            catalan[i] += (catalan[j] * catalan[i-j-1]) % MOD;
+            if(catalan[i] >= MOD){
+                catalan[i] -= MOD;
+            }
+        }
+    }
+}
