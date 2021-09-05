082500423314bfa787a679ab68943badb337b603
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-09-05 18:13:03 -0300

    Create mergeSortTree.cpp

---
+++ Data Structure/mergeSortTree.cpp
@@ -0,0 +1,30 @@
+//We want to answer queries of the following form: for three given numbers (l,r,x)
+//we have to find the minimal number in the segment a[l…r] which is greater than or equal to x.
+
+vector<int> t[4*MAXN];
+
+void build(int a[], int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v] = vector<int>(1, a[tl]);
+    } else { 
+        int tm = (tl + tr) / 2;
+        build(a, v*2, tl, tm);
+        build(a, v*2+1, tm+1, tr);
+        merge(t[v*2].begin(), t[v*2].end(), t[v*2+1].begin(), t[v*2+1].end(),
+              back_inserter(t[v]));
+    }
+}
+
+int query(int v, int tl, int tr, int l, int r, int x) {
+    if (l > r)
+        return INF;
+    if (l == tl && r == tr) {
+        vector<int>::iterator pos = lower_bound(t[v].begin(), t[v].end(), x);
+        if (pos != t[v].end())
+            return *pos;
+        return INF;
+    }
+    int tm = (tl + tr) / 2;
+    return min(query(v*2, tl, tm, l, min(r, tm), x), 
+               query(v*2+1, tm+1, tr, max(l, tm+1), r, x));
+}
