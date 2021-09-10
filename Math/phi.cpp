67924731314d6630fac413e053f11713ec148b2e
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-09-10 19:20:16 -0300

    Update phi.cpp

--- Math/phi.cpp
+++ Math/phi.cpp
@@ -3,6 +3,22 @@ using namespace std;
 typedef long long ll;
 const int LIM = 1e6 + 5;
 
+//Calculating phi in sqrt(n)
+int calc_phi(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
