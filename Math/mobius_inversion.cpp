75a0f77c96b0ed0eddfb24d37aa67eda424f30f7
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-10-16 19:16:54 -0300

    Create mobius_inversion.cpp

---
+++ Math/mobius_inversion.cpp
@@ -0,0 +1,14 @@
+const int MAXN = 1e5 + 5;
+
+vector<bool> isPrime(MAXN, false);
+vector<int> u(MAXN, 1);
+
+for(int i = 2; i < MAXN; i++){
+    if(isPrime[i]){
+        for(int j = i; j < MAXN; j += i){
+            if(j > i) isPrime[j] = 0;
+            if(j % (i*i) == 0) u[j] = 0;
+            u[j] = -u[j];
+        }
+    }
+}
