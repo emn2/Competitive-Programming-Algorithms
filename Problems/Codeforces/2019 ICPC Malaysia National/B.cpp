62b8dbcc284ecd198a02a0b8044b58fda7e3f2ea
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-10-16 23:18:47 -0300

    Create B.cpp

---
+++ Problems/Codeforces/2019 ICPC Malaysia National/B.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int w, h;
+        cin >> w >> h;
+        if(w==h) cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
