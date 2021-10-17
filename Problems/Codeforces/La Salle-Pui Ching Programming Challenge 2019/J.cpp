7c6b8c55f766a8237b338f44f7d32dc38eda46f8
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-10-16 22:59:48 -0300

    Create J.cpp

---
+++ Problems/Codeforces/La Salle-Pui Ching Programming Challenge 2019/J.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    cout << ceil(3.5 * n) << '\n';
+    
+    return 0;
+}
