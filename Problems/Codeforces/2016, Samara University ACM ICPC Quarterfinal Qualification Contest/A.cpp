5e49447d13ffef1a8a1e1d5a8028903d70c6c60a
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-10-16 23:29:10 -0300

    Create A.cpp

---
+++ Problems/Codeforces/2016, Samara University ACM ICPC Quarterfinal Qualification Contest/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;cin>>n;
+    int mx=0;
+    for(int i=0;i<n;++i){
+        int x;cin>>x;
+        mx = max(mx, x);
+    }
+    cout<<mx<<'\n';
+    return 0;
+}
