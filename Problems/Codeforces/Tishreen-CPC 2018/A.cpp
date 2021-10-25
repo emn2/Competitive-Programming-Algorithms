cf893bc5c7da5fb820d38ad5d605753c80c725b1
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-10-25 12:57:27 -0300

    Create A.cpp

---
+++ Problems/Codeforces/Tishreen-CPC 2018/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int s, d;
+        cin >> s >> d;
+        if(s >= d) cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
