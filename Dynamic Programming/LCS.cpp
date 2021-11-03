529cc131dac76ad734bd4bb1a9c063f636266771
Edson Neto <72220739+emn2@users.noreply.github.com>
2021-11-03 00:29:26 -0300

    Update LCS.cpp

--- Dynamic Programming/LCS.cpp
+++ Dynamic Programming/LCS.cpp
@@ -1,28 +1,45 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int lcs(string s1, string s2, int m, int n){
-    int L[m+1][n+1];
+const int MAXN = 3005;
+
+int L[MAXN][MAXN];
+
+string restore_answer(int sz, string &s, string &t){
+    string ans = "";
+    int i = s.size(), j = t.size();
+    while(i > 0 && j > 0){
+        if(s[i - 1] == t[j - 1]){
+            ans += s[i - 1];
+            i--, j--;
+        }
+        else if(L[i-1][j] > L[i][j-1]) i--;
+        else j--;
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    return ans;
+}
+
+string lcs(string &s, string &t, int m, int n){
     for(int i = 0; i <= m; i++){
         for(int j = 0; j <= n; j++){
-            if(i == 0 || j == 0){
-                L[i][j] = 0;
-            }
-            else if(s1[i-1] == s2[j-1]){
-                L[i][j] = L[i-1][j-1] + 1; 
-            }
-            else{
-                L[i][j] = max(L[i-1][j], L[i][j-1]);
-            }
+            if(i == 0 || j == 0) L[i][j] = 0;
+            else if(s[i-1] == t[j-1]) L[i][j] = L[i-1][j-1] + 1;
+            else L[i][j] = max(L[i-1][j], L[i][j-1]);
         }
     }
-    return L[m][n];
+
+    return restore_answer(L[m][n], s, t);
 }
 
 int main(){
-    string s1, s2;
-    while(getline(cin, s1) && getline(cin, s2)){
-        cout << lcs(s1, s2, s1.size(), s2.size()) << '\n';
-    }
-    return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s, t;
+    cin >> s >> t;
+
+    cout << lcs(s, t, s.size(), t.size()) << '\n';
 }
