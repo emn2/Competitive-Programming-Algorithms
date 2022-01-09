56fa63146c0fcee9b78daaa9bbc09fcd00039eab
Edson Neto <72220739+emn2@users.noreply.github.com>
2022-01-09 19:07:04 -0300

    Update rabinKarp.cpp

--- Strings/rabinKarp.cpp
+++ Strings/rabinKarp.cpp
@@ -41,3 +41,21 @@ private:
             h[i] = (h[i - 1] * base + s[i]) % mod;
     }
 };
+
+//ou 
+
+template<class F = unsigned int, class T = string>
+struct RabinKarp{
+    int n;
+    vector<F> p, pw;
+    F C[2] = {727, 137};
+    RabinKarp(){}
+    //change C id for double hashing
+    RabinKarp(const T& s, bool id):n((int)s.size()),p(n+1),pw(n+1,1){
+        for(int i = 1; i <= n; i++){
+            pw[i] = pw[i-1]*C[id];
+            p[i] = p[i-1]*C[id] + s[i-1];
+        }
+    }
+    F hash(int i, int len){return p[i+len] - pw[len]*p[i];}
+};
