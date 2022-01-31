a7db110836a9dc3cecd58bc5954a5138ea0fd2b6
Edson Neto <72220739+emn2@users.noreply.github.com>
2022-01-31 19:56:00 -0300

    Update rabinKarp.cpp

--- Strings/rabinKarp.cpp
+++ Strings/rabinKarp.cpp
@@ -59,3 +59,46 @@ struct RabinKarp{
     }
     F hash(int i, int len){return p[i+len] - pw[len]*p[i];}
 };
+
+//ou 
+
+struct hash_str {
+    int p[2] = {727, 137};
+    int MOD[2] = {1e9 + 7, 1e9 + 9};
+
+    vector<ll> h[2], pw[2];
+
+    hash_str(string s){
+        int n = s.size();
+
+        for(int i = 0; i < 2; i++){
+            h[i].resize(n);
+            pw[i].resize(n);
+        }
+
+        h[0][0] = h[1][0] = s[0];
+
+        for(int i = 1; i < n; i++){
+            h[0][i] = (h[0][i - 1]*p[0] + s[i]) % MOD[0];
+            h[1][i] = (h[1][i - 1]*p[1] + s[i]) % MOD[1];
+        }
+
+        pw[0][0] = pw[1][0] = 1;
+        for(int i = 1; i < n; i++){
+            pw[0][i] = (pw[0][i - 1]*p[0]) % MOD[0];
+            pw[1][i] = (pw[1][i - 1]*p[1]) % MOD[1];
+        }
+    }
+
+    pair<ll, ll> sub_hash(int l, int r){
+        if(l == 0) return make_pair(h[0][r], h[1][r]);
+
+        ll ans_0 = (h[0][r] - h[0][l - 1]*pw[0][r - l + 1]) % MOD[0];
+        ll ans_1 = (h[1][r] - h[1][l - 1]*pw[1][r - l + 1]) % MOD[1];
+
+        if(ans_0 < 0) ans_0 += MOD[0];
+        if(ans_1 < 0) ans_1 += MOD[1];
+
+        return make_pair(ans_0, ans_1);
+    };
+};
